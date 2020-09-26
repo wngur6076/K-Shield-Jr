d048e60543798354fc7eb20f03c6c91ce076920c
wngur6076 <wngur6076@naver.com>
2020-09-27 06:19:44 +0900

    test1

--- 크롤링연습/김주혁/main.py
+++ 크롤링연습/김주혁/main.py
@@ -9,34 +9,35 @@ def get_form_list(url):
         html = req.text
         soup = BeautifulSoup(html,'html.parser')
 
-    from_list = [];
+    from_list = list()
     # <from> </form> tag find_all
     for form_tag in soup.find_all('form'):
         # <input> </input> tag find_all
-        input_list = [];
+        input_list = list()
         for input_tag in form_tag.find_all('input'):
-            # <input type='submit' and disabled='disabled' continue'></input>
+            # <input type='submit' or disabled='disabled' 이면 continue'></input>
             if (input_tag.get('type') == 'submit') or (input_tag.get('disabled') == 'disabled'):
-                continue;
-            input_list.append([input_tag.get('name'), input_tag.get('value')]);
+                continue
+            input_list.append([input_tag.get('name'), input_tag.get('value')])
 
-        form_method = form_tag.get('method');
-        form_method == None and 'get' or form_method;
-        from_list.append(Form(form_tag.get('action'), form_method, dict(input_list)));
+        form_method = form_tag.get('method')
+        form_method == None and 'get' or form_method
+        from_list.append(Form(form_tag.get('action'), form_method, dict(input_list)))
         
-    return from_list;
+    return from_list
+
+
 
 
 if __name__=="__main__":
-    # url = input("url: ");
+    # url = input("url: ")
 
     # Test url:
     # http://namucoffee.com/
     # https://sir.kr/
     url = 'http://namucoffee.com/'
-    form_list = get_form_list(url);
-
+    form_list = get_form_list(url)
     for form in form_list:
-        print(form.action);
-        print(form.method);
-        print(form.parameter);
+        print(form.action)
+        print(form.method)
+        print(form.get_parameter())
--- 크롤링연습/김주혁/test.py
+++ 크롤링연습/김주혁/test.py
@@ -1,10 +1,10 @@
 class Form:
 
-
     def __init__(self, action, method, parameter):
-        self.__action = action;
-        self.__method = method;
-        self.__parameter = parameter;
+        self.__action = action
+        self.__method = method
+        self.__parameter = parameter
+        self.__query = 'default'
 
     @property
     def action(self):
@@ -21,8 +21,22 @@ class Form:
         self.__method = method
 
     @property
-    def parameter(self):
-        return self.__parameter
-    @parameter.setter    
-    def parameter(self, parameter):
-        self.__parameter = parameter
+    def query(self):
+        return self.__query
+    @query.setter    
+    def query(self, query):
+        self.__query = query
+    
+    def get_parameter(self):
+        if self.__method == 'get':       
+            url = str()
+            for key, val in self.__parameter.items():
+                url = url + '&' + key + '=' + (val == None and self.__query or val)
+            
+            return url.replace('&', '?', 1)
+        
+        if self.__method == 'post':
+            info = list()
+            for key, val in self.__parameter.items():
+                info.append([key, val == None and self.__query or val])
+            return dict(info)
